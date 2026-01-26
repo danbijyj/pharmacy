4070933be0431ac8cf5c2474e5f5611921243792
danbijyj3 <danbijyj3@gmail.com>
2026-01-26 16:23:51 +0900

    Update pharmacy.js

--- src/assets/api/pharmacy.js
+++ src/assets/api/pharmacy.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
-const BASE_URL = '/api/B552657/ErmctInsttInfoInqireService/getParmacyFullDown';
+const BASE_URL =
+    'https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown';
 
 export const fetchPharmacies = async ({ pageNo = 1, numOfRows = 25200 }) => {
+    const serviceKey = import.meta.env.VITE_PHARMACY_API_KEY;
+
     const res = await axios.get(BASE_URL, {
         params: {
-            serviceKey: import.meta.env.VITE_PHARMACY_API_KEY,
+            serviceKey,
             pageNo,
             numOfRows,
             _type: 'json',
